8d3fb16825e5ca4a9de83193f7071effc3f556fc
Ethan Bourguigneau <ethan.bourguigneau@etu.univ-grenoble-alpes.fr>
2022-02-28 15:28:17 +0100

    Avancement Js

--- TP1/javascript.js
+++ TP1/javascript.js
@@ -11,3 +11,14 @@ console.log(boutton)
 for(var i=0; i< boutton.length; i++){
     boutton[i].addEventListener('click',incrementPanier)
 }
+
+
+var nav= document.querySelectorAll(".navet");
+
+function chgNav(x){
+    document.querySelector(".active").classList.remove("active")
+}
+
+for(var i=0; i< nav.length; i++){
+    boutton[i].addEventListener('click',chgNav(boutton[i]))
+}
