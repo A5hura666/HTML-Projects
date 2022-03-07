7c001d8a4aacbc8a5f8ce51794ff3f4c47f34858
Yohann <73314529+yohann69@users.noreply.github.com>
2022-03-07 20:22:04 +0100

    Easter Egg

--- TP1/javascript.js
+++ TP1/javascript.js
@@ -1,12 +1,6 @@
 let boutton = document.querySelectorAll(".btn");
 let nbarticle=0
 
-function easter_egg(){
-    if (nbarticle==10){
-        video.classList.add("easter");
-    }
-}
-
 function incrementPanier() {
     nbarticle++;
     document.querySelector(".nb_article").innerHTML = nbarticle
@@ -19,4 +13,10 @@ for(let i=0; i< boutton.length; i++){
     boutton[i].addEventListener('click',incrementPanier)
 }
 
+
 let video = document.querySelector("body iframe");
+function easter_egg(){
+    if (nbarticle==10){
+        document.querySelector("body").innerHTML += `<iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&showinfo=0&controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+    }
+}
